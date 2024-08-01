d96a9fd23fda862dc526a47ac1390f58ed7be34c
saurabhpandey <saurabhdaju.coder@gmail.com>
2024-08-01 17:56:46 +0530

    implement GUI

--- Client.java
+++ Client.java
@@ -1,6 +1,6 @@
 import java.net.*;
+import java.io.*;
 
-import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -11,8 +11,6 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
-import javax.swing.event.DocumentListener;
-import javax.swing.*;
 import javax.swing.text.*;
 import javax.swing.event.*;
 
@@ -23,7 +21,6 @@ import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
-import java.io.*;
 
 public class Client extends JFrame {
 
@@ -33,7 +30,7 @@ public class Client extends JFrame {
     PrintWriter out;
 
     // Declaring GUI Components
-    private JLabel heading = new JLabel("daju");
+    private JLabel heading = new JLabel("Server");
     private JTextArea msgsArea = new JTextArea();
     private JTextField msgInput = new JTextField();
     private Font font = new Font("Roboto", Font.PLAIN, 20);
@@ -51,13 +48,13 @@ public class Client extends JFrame {
             createGUI();
             handleEvents();
             reader();
-            // writer();
 
         } catch (Exception e) {
             if (e instanceof ConnectException) {
                 System.out.println("HTTP Status Code 503 - S E R V E R  U N A V A I L A B L E \n");
             } else {
                 e.printStackTrace();
+                System.out.println("Client Constructor Error\n");
             }
         }
     }
@@ -71,13 +68,12 @@ public class Client extends JFrame {
 
         // setting the layout
         this.setLayout(new BorderLayout());
-        // Border layout divides our window in 5 parts- North South East West Center.
-        // default- center
+        // Border layout divides our window in 5 parts- North South East West Center. (default- center)
 
         // heading
         this.add(heading, BorderLayout.NORTH);
         heading.setFont(font);
-        ImageIcon dp = new ImageIcon("./dpC.jpg");
+        ImageIcon dp = new ImageIcon("./dpS.png");
         BufferedImage img = new BufferedImage(dp.getImage().getWidth(null), dp.getImage().getHeight(null),
                 BufferedImage.TYPE_INT_ARGB);
         img.getGraphics().drawImage(dp.getImage(), 0, 0, null); // makes a copy of the image to avoid any changes to the
@@ -89,13 +85,12 @@ public class Client extends JFrame {
         heading.setVerticalTextPosition(SwingConstants.BOTTOM);
         heading.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
 
-        // message area
+        // message display area
         JScrollPane scrollPane = new JScrollPane(msgsArea, JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); //add a scrollable view of messages-display-area, hides the horizontal and vertical scrollbars
         this.add(scrollPane, BorderLayout.CENTER); //add the scrollable view to the center of the frame
         msgsArea.setFont(font);
         msgsArea.setBackground(Color.LIGHT_GRAY);
         msgsArea.setEditable(false);
-        msgsArea.setCaretColor(Color.BLUE);
 
         // add auto-scrolling to the bottom of the messages-display-area
         Document document = msgsArea.getDocument();
@@ -106,9 +101,7 @@ public class Client extends JFrame {
             }
 
             @Override
-            public void removeUpdate(DocumentEvent e) {
-                // Handle potential removal scenarios if needed
-            }
+            public void removeUpdate(DocumentEvent e) {}
 
             @Override
             public void changedUpdate(DocumentEvent e) {
@@ -133,32 +126,26 @@ public class Client extends JFrame {
         msgInput.addKeyListener(new KeyListener() {
 
             @Override
-            public void keyTyped(KeyEvent e) {
-                // throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
-            }
+            public void keyTyped(KeyEvent e) {}
 
             @Override
-            public void keyPressed(KeyEvent e) {
-                // throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
-            }
+            public void keyPressed(KeyEvent e) {}
 
             @Override
             public void keyReleased(KeyEvent e) {
                 if (e.getKeyCode() == 10) {
-                    String contentToSend = msgInput.getText();
-                    System.out.println("You tried to send msg: " + contentToSend);
+                    String contentToSend = msgInput.getText();  
 
                     if(contentToSend.equals("sudo exit")) {
                         out.println(contentToSend);
                         out.flush();
-                        System.out.println("C O N N E C T I O N   I N T E R R U P T E D\n");
                         JOptionPane.showMessageDialog(Client.this, "You end the chat.\n");
                         msgInput.setEnabled(false);
                         try {
                             socket.close();
                         } catch (Exception exception) {
                             if (exception instanceof SocketException) {
-                                // System.out.println("C O N N E C T I O N  C L O S E D\n");
+                                System.out.println("C O N N E C T I O N  C L O S E D\n");
                             } else {
                                 exception.printStackTrace();
                             }
@@ -167,7 +154,7 @@ public class Client extends JFrame {
                     }
                     else if(!(contentToSend.length() == 0
                             || (contentToSend.chars().distinct().count() == 1 && contentToSend.charAt(0) == ' '))) {
-                        msgsArea.append("You: " + contentToSend + "\n");
+                        msgsArea.append("You     : " + contentToSend + "\n");
                         out.println(contentToSend);
                         out.flush();
 
@@ -187,21 +174,22 @@ public class Client extends JFrame {
                     String msg = br.readLine();
 
                     if (msg.equals("sudo exit")) {
-                        System.out.println("Server terminated the chat.\n");
-                        System.out.println("C O N N E C T I O N   I N T E R R U P T E D\n");
-
                         JOptionPane.showMessageDialog(this, "Server terminated the chat.\n");
                         msgInput.setEnabled(false);
 
                         socket.close();
                         break;
                     }
-                    msgsArea.append("Server: " + msg + "\n");
-                    // System.out.println("Server: " + msg + "\n");
-                } catch (Exception e) {
+                    msgsArea.append("Server : " + msg + "\n");
+                } 
+                catch (Exception e) {
                     if (e instanceof NullPointerException) {
                         System.out.println("[ R e a d e r  S t o p p e d ]\n");
-                    } else {
+                    } 
+                    else if(e instanceof SocketException) {
+                        System.out.println("Chat ended by Server\n");
+                    }
+                    else {
                         e.printStackTrace();
                     }
                     return;
@@ -213,41 +201,9 @@ public class Client extends JFrame {
         new Thread(r1).start();
     }
 
-    public void writer() {
-        if (socket.isClosed())
-            return;
-
-        Runnable r2 = () -> {
-            System.out.println("[ W r i t e r  S t a r t e d ]\n");
-            while (!socket.isClosed()) {
-                try {
-                    BufferedReader clientInput = new BufferedReader(new InputStreamReader(System.in));
-                    String msg = clientInput.readLine();
-                    out.println(msg);
-                    out.flush();
-                    System.out.println();
-
-                    if (msg.equals("sudo exit")) {
-                        socket.close();
-                        break;
-                    }
-                } catch (Exception e) {
-                    System.out.println("WRITER KA CATCH BLOCK HIT HUA HAI");
-                    e.printStackTrace();
-                    System.out.println("WRITER KA CATCH BLOCK KHATAM");
-                }
-            }
-            System.out.println("C O N N E C T I O N   C L O S E D");
-        };
-
-        // Starting the thread
-        new Thread(r2).start();
-    }
-
     public static void main(String[] args) {
         System.out.println("This is Client...going to start Client\n");
 
         new Client();
     }
-
 }
--- Server.java
+++ Server.java
@@ -1,7 +1,28 @@
-import java.io.*;
 import java.net.*;
+import java.io.*;
 
-class Server {
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.text.*;
+import javax.swing.event.*;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+
+class Server extends JFrame {
 
     ServerSocket serverSocket;
     Socket clientSocket;
@@ -15,22 +36,133 @@ class Server {
             serverSocket = new ServerSocket(7777);
             System.out.println("Server is running on Port 7777\n");
             System.out.println("Server is ready to accept connection\n");
-            System.out.println("Waiting...\n");
 
+            createGUI();
             clientSocket = serverSocket.accept();
+            JOptionPane.showMessageDialog(this, "Client Connected Successfully");
 
             //For reading-writing data through client socket
             br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out = new PrintWriter(clientSocket.getOutputStream(), true);
 
+            handleEvents();
             reader();
-            writer();
         } 
         catch (IOException e) {
-            System.out.println("SERVER CONSTRUCTOR KA CATCH BLOCK HIT HUA HAI");
             e.printStackTrace();
-            System.out.println("SERVER CONSTRUCTOR KA CATCH BLOCK KHATAM");
+            System.out.println("Server Constructor Error\n");
         }
+    }
+
+
+    // Declaring GUI Components
+    private JLabel heading = new JLabel("Client");
+    private JTextArea msgsArea = new JTextArea();
+    private JTextField msgInput = new JTextField();
+    private Font font = new Font("Roboto", Font.PLAIN, 20);
+
+    private void createGUI() {
+
+        this.setTitle("tadow");
+        this.setSize(600, 700);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.setLayout(new BorderLayout());
+
+        //heading properties
+        this.add(heading, BorderLayout.NORTH);
+        heading.setFont(font);
+        ImageIcon dp = new ImageIcon("./dpC.jpg");
+        BufferedImage img = new BufferedImage(dp.getImage().getWidth(null), dp.getImage().getHeight(null),
+                BufferedImage.TYPE_INT_ARGB);
+        img.getGraphics().drawImage(dp.getImage(), 0, 0, null); // makes a copy of the image to avoid any changes to the original image
+        Image image = img.getScaledInstance(45, 45, Image.SCALE_SMOOTH);
+        dp = new ImageIcon(image);
+        heading.setIcon(dp);
+        heading.setHorizontalAlignment(SwingConstants.CENTER);
+        heading.setVerticalTextPosition(SwingConstants.BOTTOM);
+        heading.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+
+        // message display area
+        JScrollPane scrollPane = new JScrollPane(msgsArea, JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); //add a scrollable view of messages-display-area, hides the horizontal and vertical scrollbars
+        this.add(scrollPane, BorderLayout.CENTER); //add the scrollable view to the center of the frame
+        msgsArea.setFont(font);
+        msgsArea.setBackground(Color.LIGHT_GRAY);
+        msgsArea.setEditable(false);
+
+        // add auto-scrolling to the bottom of the messages-display-area
+        Document document = msgsArea.getDocument();
+        document.addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                scrollToEnd();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {}
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                scrollToEnd();
+            }
+
+            private void scrollToEnd() {
+                SwingUtilities.invokeLater(() -> {
+                    msgsArea.setCaretPosition(msgsArea.getDocument().getLength());
+                });
+            }
+        });
+
+        // message input area
+        this.add(msgInput, BorderLayout.SOUTH);
+        msgInput.setFont(font);
+
+        this.setVisible(true);
+    }
+
+    private void handleEvents() {
+        msgInput.addKeyListener(new KeyListener() {
+
+            @Override
+            public void keyTyped(KeyEvent e) {}
+
+            @Override
+            public void keyPressed(KeyEvent e) {}
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == 10) {
+                    String contentToSend = msgInput.getText();
+
+                    if(contentToSend.equals("sudo exit")) {
+                        out.println(contentToSend);
+                        out.flush();
+                        JOptionPane.showMessageDialog(Server.this, "You end the chat.\n");
+                        msgInput.setEnabled(false);
+                        try {
+                            clientSocket.close();
+                        } catch (Exception exception) {
+                            if (exception instanceof SocketException) {
+                                System.out.println("You end the chat.\n");
+                            } else {
+                                exception.printStackTrace();
+                            }
+                        }
+                        return;
+                    }
+                    else if(!(contentToSend.length() == 0
+                            || (contentToSend.chars().distinct().count() == 1 && contentToSend.charAt(0) == ' '))) {
+                        msgsArea.append("You    : " + contentToSend + "\n");
+                        out.println(contentToSend);
+                        out.flush();
+
+                        msgInput.setText("");
+                        msgInput.requestFocus();
+                    }
+                }
+            }
+        });
     }
 
     public void reader() {
@@ -43,21 +175,25 @@ class Server {
                     String msg = br.readLine();
 
                     if(msg.equals("sudo exit")) {
-                        System.out.println("Client Terminated the chat\n");
-                        System.out.println("C O N N E C T I O N   I N T E R R U P T E D\n");
+                        JOptionPane.showMessageDialog(this, "Client terminated the chat.\n");
+                        msgInput.setEnabled(false);
                         clientSocket.close();
                         break;
                     }
 
-                    System.out.println("Client: " + msg + "\n");
+                    msgsArea.append("Client : " + msg + "\n");
                 } 
                 catch(Exception e) {
-                    if(e instanceof SocketException) {
-                        // System.out.println("C O N N E C T I O N   C L O S E D\n");
+                    if(e instanceof NullPointerException) {
+                        System.out.println("[ R e a d e r  S t o p p e d ]\n");
                     } 
+                    else if(e instanceof SocketException) {
+                        System.out.println("Chat ended by Client\n");
+                    }
                     else {
                         e.printStackTrace();
                     }
+                    return;
                 }
                 
             }
@@ -67,36 +203,6 @@ class Server {
         new Thread(r1).start();
     }
 
-    public void writer() {
-            
-            //Creating a thread to write data to client
-            Runnable r2=()-> {
-                System.out.println("[ W r i t e r  S t a r t e d ]\n");
-                while(!clientSocket.isClosed()) {
-                    try {
-                        BufferedReader ServerInput = new BufferedReader(new InputStreamReader(System.in));
-                        String response = ServerInput.readLine();
-                        out.println(response);
-                        out.flush();
-                        System.out.println();
-
-                        if(response.equals("sudo exit")) {
-                            clientSocket.close();
-                            break;
-                        }
-                    }
-                    catch(Exception e) {
-                        System.out.println("WRITER KA CATCH BLOCK HIT HUA HAI");
-                        e.printStackTrace();
-                        System.out.println("WRITER KA CATCH BLOCK KHATAM");
-                    }
-                }
-                System.out.println("C O N N E C T I O N   C L O S E D\n");
-            };
-
-            //Starting the thread
-            new Thread(r2).start();
-    }
     public static void main(String[] args) {
         System.out.println("This is Server...going to start Server\n");
 
